c8d2119c28adffe298e3227e52b2cf435882e6a5
coredumpdev <muzaffertolgayakar@gmail.com>
2026-07-15 15:53:08 +0300

    capture the keyboard interrupt is ok

--- main.c
+++ main.c
@@ -4,15 +4,31 @@
 #include "net/tcp/server.h"
 #include "net/tcp/tcp.h"
 
+#include <signal.h>
+
 #include <stdlib.h>
 
 #define PORT   6379
 #define BACKLOG 512
 
+HashTable* db;
+
+void signal_handler(int signal)
+{
+    if (db != NULL){
+        ht_destroy(db);
+        LOG(stdout, "DB SILINDI");
+    }
+
+    exit(0);
+}
+
 int main(void)
 {
-    HashTable* db = ht_create();
+    db = ht_create();
 
+    signal(SIGINT, signal_handler);
+    
     int sd = create_tcp_socketd(AF_INET);
     if (sd < 0)
         return 1;
